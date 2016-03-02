1f60cbd50edbe50128d20ebe0cd22ab582958b54
semper_idem <semper_idem@126.com>
2016-03-02 19:37:02 +0800

    [#1 TapCounter]Update Project #1
    Update Project #1, finish the iOS version

---
+++ #1 TapCounter/index.ios.js
@@ -0,0 +1,153 @@
+// 严格模式，在这个模式下 JavaScript 
+// 将会采用严格的语法检查机制，避免一些语法缺陷
+'use strict';
+import React, {  // 相当于 using namespace
+  AppRegistry,
+  Component,
+  Text,
+  View,
+  Navigator,
+  TouchableHighlight
+} from 'react-native';  // 从该模块中导入相关库
+
+// 类是在 ES6 中被引入的
+// 继承自 Component（React UI 的基础模块）
+class HomePage extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      counter : 0
+    };
+  }
+
+  render() {
+    return (
+      <View style={styles.homePageContainer}>
+        <Text style={styles.number}>
+          { this.state.counter }
+        </Text>
+        <TouchableHighlight style = {styles.button}
+          underlayColor = '#99d9f4'
+          onPress = {this.onTapPressed.bind(this)}>
+          <Text style = {styles.buttonText}>点击</Text>
+        </TouchableHighlight>
+        <TouchableHighlight style = {styles.button}
+          underlayColor = '#99d9f4'
+          onPress = {this.onClean.bind(this)}>
+          <Text style = {styles.buttonText}>清空</Text>
+        </TouchableHighlight>
+      </View>
+    );
+  }
+
+  // Action
+  onTapPressed() {
+    var count = this.state.counter + 1;
+    this.setState({
+      counter: count
+    });
+    console.log('Current counter is ' + count);
+  }
+
+  onClean() {
+    this.setState({
+      counter: 0
+    });
+  }
+}
+
+class TapCounter extends Component {
+
+  render() {
+    return (
+      <Navigator
+        initialRoute = {{ name: 'Tap Counter', component: HomePage, title: 'Tap Counter' }}
+        style = {styles.container}
+        renderScene = {(route, navigator) =>
+          <HomePage title={route.title} navigator={navigator} />
+        }
+        navigationBar = {
+          <Navigator.NavigationBar 
+          routeMapper = {NavigationBarRouteMapper}
+          style = {styles.navBar}
+        />}
+      />
+    );
+  }
+}
+
+var NavigationBarRouteMapper = {
+  LeftButton: function (route, navigator, index, navState) {
+    return null;
+  },
+
+  RightButton: function (route, navigator, index, navState) {
+    return null;
+  },
+
+  Title: function (route, navigator, index, navState) {
+    return (
+      <Text style={[styles.navBarText, styles.navBarTitleText]}>
+          {route.title}
+      </Text>
+    );
+  },
+}; 
+
+// 这个定义了一个样式，使用 CSS 来进行定义
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  homePageContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  flow: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    alignSelf: 'stretch',
+  },
+  number: {
+    fontSize: 60,
+    textAlign: 'center',
+    margin: 10,
+  },
+  button: {
+    marginTop: 10,
+    marginBottom: 10,
+    marginLeft: 30,
+    marginRight: 30,
+    height: 50,
+    backgroundColor: '#48BBEC',
+    borderRadius: 8,
+    alignSelf: 'stretch',
+    justifyContent: 'center',
+  },
+  buttonText: {
+    fontSize: 20,
+    color: 'white',
+    alignSelf: 'center',
+  },
+  navBar: {
+    backgroundColor: '#F5FCFF',
+  },
+  navBarText: {
+    fontSize: 16,
+    marginVertical: 10,
+    color: '#48BBEC'
+  },
+  navBarTitleText: {
+    fontWeight: '500',
+    marginVertical: 9,
+  },
+  navBarButtonText: {
+    marginLeft: 15,
+  },
+});
+
+// AppRegistry 定义了 App 的入口，并提供了根组件
+AppRegistry.registerComponent('TapCounter', () => TapCounter);
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# 100DaysOfReactNative
-Learning Facebook's new frameworks React Native for building native apps
+# 100 Days Of React Native
+> Learning Facebook's new frameworks React Native for building native apps
+
+Since Facebook has announced [React Native](http://facebook.github.io/react-native/) as the world-class frameworks by using JaveScript and React, many programmer was excited to learn it. Because the company which I'm interested in is using React Native for developing their apps, so I decided to give it a shot. I hope I can finish this project before May.
